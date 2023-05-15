a11ea68dd631b098dc6e7a724b44ef771a8511bc
Michael Hespenheide <mahespenheide@gmail.com>
2023-05-15 18:43:09 -0400

    Remove temporary files

--- pages/api/sentry-example-api.js
+++
@@ -1,5 +0,0 @@
-// A faulty API route to test Sentry's error monitoring
-export default function handler(_req, res) {
-  throw new Error("Sentry Example API Route Error 2");
-  res.status(200).json({ name: "John Doe" });
-}
